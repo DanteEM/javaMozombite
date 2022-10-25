6db285bd411fb9e52d05f55a4a6ab0407b8cf2f1
Dante Mozombite <dantemozombitequiroz@gmail.com>
2022-10-25 01:49:10 -0500

    new start!

--- main.js
+++ main.js
@@ -1,161 +1,136 @@
-const nombredeAlumno = prompt("Introduzca su nombre")
+// declarar variables//
 
-function saludar(Nombre) {
-    alert("Bienvenido Alumno " + Nombre)
-}
-saludar(nombredeAlumno)
+const table = document.getElementById("table");
 
+const notas = document.getElementById("notas");
 
-let question = 0
-let nota1 = parseInt(prompt("Ingrese su nota del Primer Examen"));
-for (question < 5; question++;) {
-    if (nota1 <= 20) {
-        break;
-    } else {
-        nota1 = parseInt(prompt("ingrese numero del 1 al 20, porfavor"));
-    }
+const interactNotas = document.getElementById("notas");
 
-}
+var arrayNotas = [];
 
-let nota2 = parseInt(prompt("Ingrese su nota del Segundo Examen"));
-for (question < 5; question++;) {
-    if (nota2 <= 20) {
-        break;
-    } else {
-        nota2 = parseInt(prompt("ingrese numero del 1 al 20, porfavor"));
-    }
+//Declarar Class//
 
-}
-
-let nota3 = parseInt(prompt("Ingrese su nota del Tercero Examen"));
-for (question < 5; question++;) {
-    if (nota3 <= 20) {
-        break;
-    } else {
-        nota3 = parseInt(prompt("ingrese numero del 1 al 20, porfavor"));
-    }
-
-}
-
-
-
-let nota4 = parseInt(prompt("Ingrese su nota del Cuarto Examen"));
-for (question < 5; question++;) {
-    if (nota4 <= 20) {
-        break;
-    } else {
-        nota4 = parseInt(prompt("ingrese numero del 1 al 20, porfavor"));
-    }
-
-}
-
-
-function sumaYDivision(Nota1, Nota2, Nota3, Nota4) {
-    return (Nota1 + Nota2 + Nota3 + Nota4) / 4;
-}
-let resultado = sumaYDivision(nota1, nota2, nota3, nota4);
-alert("Tu calificacion final es " + resultado)
-
-resultado >= 11 ? alert("Felicidades! Estas aprobado") : alert("Mis mas sinceras penas, estas desaprobado");
-console.log(resultado <= 11 || "aprobo!");
-
-const boton = document.getElementById("notaFinal");
-
-boton.addEventListener("click", () => {
-    Swal.fire ("tu nota final es " + resultado)
-
-})
-
-
-
-
-
-
-class Notas {
-    constructor(nombre, nota) {
-        this.nombre = nombre.toUpperCase();
-        this.nota = nota
-    }
-}
-const primerExamen = new Notas("Primer Examen", nota1)
-const segundoExamen = new Notas("Segundo Examen", nota2)
-const tercerExamen = new Notas("Tercer Examen", nota3)
-const cuartoExamen = new Notas("Cuarto Examen", nota4)
-
-const arrayDeNotas = [primerExamen, segundoExamen, tercerExamen, cuartoExamen]
-
-const examenes = [];
-examenes.push(primerExamen);
-examenes.push(segundoExamen);
-examenes.push(tercerExamen);
-examenes.push(cuartoExamen);
-
-console.log(examenes);
-for (let Notas of examenes) {
-    console.log(Notas);
-}
-
-const listaDeNotas = [nota1, nota2, nota3, nota4]
-listaDeNotas.length === 4 && alert("Recuerda siempre estudiar")
-alert("Estas son las notas de tu primer hasta el ultimo examen " + listaDeNotas.join("\n"))
-
-const lstaDeNotas = document.getElementById("notas");
-
-
-arrayDeNotas.forEach(notas => {
-    let li = document.createElement("li");
-    li.innerHTML = `<p>${notas.nombre} </p>
-    <p>Nota: ${notas.nota}</p>`
-
-    lstaDeNotas.appendChild(li)
-})
-
-
-document.getElementById("Usuario").innerHTML = nombredeAlumno
-
-
-class Quejas {
-    constructor(nombre, gradoSection, queja){
+class Nota {
+    constructor(nombre, nota1, nota2, nota3, nota4, promedio) {
         this.nombre = nombre;
-        this.gradoSection = gradoSection;
-        this.queja = queja;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+        this.nota3 = nota3;
+        this.nota4 = nota4;
+        this.promedio = promedio;
     }
 }
+//Declarar Funciones//
 
-const arrayDeQuejas = [];
+const promedio = (n1, n2, n3, n4) => {
+    n1 = parseInt(n1);
+    n2 = parseInt(n2);
+    n3 = parseInt(n3);
+    n4 = parseInt(n4);
+    let p = ((n1 + n2 + n3 + n4) / 4)
+    console.log("nota final: " + p)
+    return p
+}
 
-
-const formularioQuejas =document.getElementById("formularioQuejas");
-
-
-formularioQuejas.addEventListener("submit", (e) =>{
-    e.preventDefault();
-    const nombre = document.getElementById("nombre");
-    const grado = document.getElementById("gradoSection");
-    const quejas = document.getElementById("queja");
-    const quejaCompleta = new Quejas(nombre.value, grado.value, quejas.value)
-    arrayDeQuejas.push(quejaCompleta)
-    console.log(arrayDeQuejas);
-formularioQuejas.reset();
-})
-
-formularioQuejas.onclick = () => {
+interactNotas.onclick = () => {
     console.log("interactuo con el formulario");
 }
 
-localStorage.setItem ("Alumno", nombredeAlumno )
-localStorage.setItem ("examen 1", nota1 )
-localStorage.setItem ("examen 2", nota2 )
-localStorage.setItem ("examen 3", nota3 )
-localStorage.setItem ("examen 4", nota4 )
+const generarTabla = () => {
+    console.log(arrayNotas);
+    const tableBody = table.querySelector("tbody")
+    tableBody.innerHTML = ""
+    arrayNotas.map((itemNota) => {
+        const itemTable = `
+    <td>
+        ${itemNota.nombre}
+    </td>
+    <td>
+    ${itemNota.nota1}
+    </td>
+    <td>
+    ${itemNota.nota2}
+    </td>
+    <td>
+    ${itemNota.nota3}
+    </td>
+    <td>
+    ${itemNota.nota4}
+    </td>
+    <td>
+    ${itemNota.promedio}
+    </td>`
 
-let localS = [
-    localStorage.getItem ("Alumno"),
-    localStorage.getItem ("examen 1"),
-    localStorage.getItem ("examen 2"),
-    localStorage.getItem ("examen 3"),
-    localStorage.getItem ("examen 4")
-]
+        let td = document.createElement("tr")
 
-console.log(localS);
+        td.innerHTML = itemTable
 
+        tableBody.appendChild(td)
+
+    })
+
+}
+
+
+const enviarNotas = (event) => {
+    event.preventDefault();
+    const {
+        nombre,
+        nota1,
+        nota2,
+        nota3,
+        nota4
+    } = event.target;
+
+    const calculetPromedio = promedio(nota1.value, nota2.value, nota3.value, nota4.value);
+
+    calculetPromedio >= 11 ? Swal.fire ("Felicidades, aprobaste!") : Swal.fire("Mis mas sinceras penas, estas desaprobado");
+
+
+    const nuevaNota = new Nota(nombre.value, nota1.value, nota2.value, nota3.value, nota4.value, calculetPromedio)
+
+    arrayNotas.push(nuevaNota)
+
+    generarTabla();
+
+
+    localStorage.setItem("Alumno", nombre.value)
+    localStorage.setItem("examen 1", nota1.value)
+    localStorage.setItem("examen 2", nota2.value)
+    localStorage.setItem("examen 3", nota3.value)
+    localStorage.setItem("examen 4", nota4.value)
+
+    let localS = [
+        localStorage.getItem("Alumno"),
+        localStorage.getItem("examen 1"),
+        localStorage.getItem("examen 2"),
+        localStorage.getItem("examen 3"),
+        localStorage.getItem("examen 4")
+    ]
+
+    console.log(localS);
+
+    sessionStorage.setItem("Alumno", nombre.value)
+    sessionStorage.setItem("examen 1", nota1.value)
+    sessionStorage.setItem("examen 2", nota2.value)
+    sessionStorage.setItem("examen 3", nota3.value)
+    sessionStorage.setItem("examen 4", nota4.value)
+
+    const url = 'https://jsonplaceholder.typicode.com/users'
+
+    fetch(url)
+    .then(Response => Response.json())
+    .then (data => {
+        console.log(data);
+    })
+    .catch (err=> console.log(err));
+
+
+
+}
+
+
+
+//declarar Eventos//
+
+notas.addEventListener("submit", enviarNotas)
